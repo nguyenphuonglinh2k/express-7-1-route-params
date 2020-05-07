8031dfa9575547a258b7c39a38db48b590e0a006
Glitch (hello-express) <none>
2020-05-07 09:55:10 +0000

    🛰🐴 Checkpoint ./server.js:15927824/340 ./views/index.pug:15927824/68 ./package.json:15927824/18

--- server.js
+++ server.js
@@ -6,6 +6,20 @@
 const express = require('express');
 const app = express();
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/todos', function(req, res) {
+  res.render('index.pug', {
+    lists: [
+      'Đi chợ',
+      'Nấu cơm',
+      'Rửa bát',
+      'Học tại CodersX'
+    ]
+  });
+});
+
 // https://expressjs.com/en/starter/basic-routing.html
 app.get('/', (request, response) => {
   response.send('I love CodersX');
